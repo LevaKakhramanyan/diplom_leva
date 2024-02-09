4e5bd769709a552cc2731649e55e7fcaa9849766
LevaKakhramanyan <puma24513@rambler.ru>
2024-02-09 15:13:01 +0000

    Задание на базу данных и автотест

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://a908625e-9132-4a71-95df-3ccc00a0a50b.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Левон",
+    "lastName": "Кахраманян",
+    "address": "Сooperative street,3",
+    "metroStation": 10,
+    "phone": "+79637265660",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-08",
+    "comment": "Привет!",
+    "color": [
+        "BLACK"
+    ]
+}
